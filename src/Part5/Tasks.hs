dc5d4d27763d4b5438efc95c1f49b944e28b7cb1
jen <esamkaeva@sotsbi.ru>
2026-01-14 18:43:44 +0300

    task 5

--- src/Part5/Tasks.hs
+++ src/Part5/Tasks.hs
@@ -4,29 +4,35 @@ import Util(notImplementedYet)
 
 -- Реализуйте левую свёртку
 myFoldl :: (b -> a -> b) -> b -> [a] -> b
-myFoldl = notImplementedYet
+myFoldl _ result [] = result
+myFoldl f result (x : xs) = myFoldl f (f result x) xs
 
 -- Реализуйте правую свёртку
 myFoldr :: (a -> b -> b) -> b -> [a] -> b
-myFoldr = notImplementedYet
+myFoldr _ result [] = result
+myFoldr f result (x : xs) = f x (myFoldr f result xs)
 
 -- Используя реализации свёрток выше, реализуйте все остальные функции в данном файле
 
 myMap :: (a -> b) -> [a] -> [b]
-myMap f = notImplementedYet
+myMap f = myFoldr (\x acc -> f x : acc) []
 
 myConcatMap :: (a -> [b]) -> [a] -> [b]
-myConcatMap f = notImplementedYet
+myConcatMap f = myFoldr (\x acc -> f x ++ acc) []
 
 myConcat :: [[a]] -> [a]
-myConcat = notImplementedYet
+myConcat = myFoldr (++) []
 
 myReverse :: [a] -> [a]
-myReverse = notImplementedYet
+myReverse = myFoldl (flip(:)) []
 
 myFilter :: (a -> Bool) -> [a] -> [a]
-myFilter p = notImplementedYet
+myFilter p = myFoldr (\x acc -> if p x then x : acc else acc) []
 
 myPartition :: (a -> Bool) -> [a] -> ([a], [a])
-myPartition p = notImplementedYet
+myPartition p = myFoldr select ([], [])
+  where
+    select x (trues, falses)
+        | p x       = (x : trues, falses)
+        | otherwise = (trues, x : falses)
 
